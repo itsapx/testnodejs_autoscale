82e8bf02d24536c6e413a6ac98c912fd30ffdcb8
Ambar Hasbiyatmoko <sap.express.it@gmail.com>
2022-03-20 03:19:01 +0700

    7

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,10 +6,10 @@
 
 
 #create our working directory if ot doesnt exist
-DIR="/home/ubuntu/nodejs-express"
-if [ -d "$DIR" ]; then
-	echo "${DIR} exists"
-else
-	echo "Creating ${DIR} directory"
-	mkdir ${DIR}
-fi
+# DIR="/home/ubuntu/nodejs-express"
+# if [ -d "$DIR" ]; then
+# 	echo "${DIR} exists"
+# else
+# 	echo "Creating ${DIR} directory"
+# 	mkdir ${DIR}
+# fi
