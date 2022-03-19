6bf9f6946c2cb6fd857d6f11c1d82dd6695ed5a9
Ambar Hasbiyatmoko <sap.express.it@gmail.com>
2022-03-20 04:21:23 +0700

    15

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,10 +6,10 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
 
 #create our working directory if ot doesnt exist
-# DIR="/home/ubuntu/nodejs-express"
-# if [ -d "$DIR" ]; then
-# 	echo "${DIR} exists"
-# else
-# 	echo "Creating ${DIR} directory"
-# 	mkdir ${DIR}
-# fi
+DIR="/home/ubuntu/nodejs-express"
+if [ -d "$DIR" ]; then
+	echo "${DIR} exists"
+else
+	echo "Creating ${DIR} directory"
+	mkdir ${DIR}
+fi
