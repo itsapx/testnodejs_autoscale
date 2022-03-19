e4330a218a9c312b7aaaba44b21e5e6ee5308e34
Ambar Hasbiyatmoko <sap.express.it@gmail.com>
2022-03-20 05:59:30 +0700

    35

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,14 +4,15 @@ sudo chmod -R 777 /home/ubuntu/nodejs-express
 cd /home/ubuntu/nodejs-express
 
 #add npm and node to path
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # loads nvm bash_completion (node is in)
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # loads nvm bash_completion (node is in)
 
 #install node modules
 #sudo npm install
-#sudo npm install pm2 -g
-sudo npm install express
-#pm2 start npm --name "myApp"
+# sudo npm install pm2 -g
+# sudo npm install express
+# pm2 start npm --name "myApp"
+
 #start our node in the background
 sudo node app.js  > app.out.log 2> app.err.log < /dev/null &
