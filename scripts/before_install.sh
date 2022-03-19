171bd0d5195d17ceab7806d874db04db8ffe00bb
Ambar Hasbiyatmoko <sap.express.it@gmail.com>
2022-03-20 03:18:20 +0700

    6

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash 
-. ~/.nvm/nvm.sh
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash 
+# . ~/.nvm/nvm.sh
 
 
 #create our working directory if ot doesnt exist
