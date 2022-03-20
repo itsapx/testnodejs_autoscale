fdb5576e702774f017171d932bd97db406dc0c6c
Ambar Hasbiyatmoko <sap.express.it@gmail.com>
2022-03-20 15:38:04 +0700

    62

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #download node and npm
-sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 16
+#sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+#. ~/.nvm/nvm.sh
+#nvm install 16
 
-npm install forever -g
+sudo npm install forever -g
