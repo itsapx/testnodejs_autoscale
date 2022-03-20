fdefb8792b13271c09fd6fe6120cbc35ca4a5786
Ambar Hasbiyatmoko <sap.express.it@gmail.com>
2022-03-20 14:41:10 +0700

    53

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Stop all servers and start the server as a daemon
-forever stopall
-forever start /home/ubuntu/nodejs-express/app.js
+sudo forever stopall
+sudo forever start /home/ubuntu/nodejs-express/app.js > app.out.log 2> app.err.log < /dev/null &
