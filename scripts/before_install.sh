19ad5534a5e0a1a5186d8a45f13a96a1e9b7342f
Ambar Hasbiyatmoko <sap.express.it@gmail.com>
2022-03-20 14:00:27 +0700

    45

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-# Install node.js
-sudo apt-get install python-software-properties -y
-sudo apt-add-repository ppa:chris-lea/node.js -y
-sudo apt-get update
-sudo apt-get install nodejs -y
+#download node and npm
+sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
 
-# Install nodemon
-# sudo npm install nodemon -g
-
-# Install forever module 
-# https://www.npmjs.com/package/forever
 sudo npm install forever -g
-
-# Clean working folder
-# sudo find /home/ubuntu/test -type f -delete
